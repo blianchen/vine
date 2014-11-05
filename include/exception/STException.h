f4d64f27108a362f27f94d0cd2da15a819b8db43
blc <blianchen@qq.com>
2014-11-05 21:48:16 +0800

    move exception to dir

---
+++ include/exception/STException.h
@@ -0,0 +1,16 @@
+#ifndef STEXCEPTION_INCLUDED
+#define STEXCEPTION_INCLUDED
+#include <exception/exception.h>
+
+
+/**
+ * Thrown to indicate that a memory allocation failed. Every object
+ * constructor method may throw a MemoryException if the underlying
+ * allocator failed.
+ * @see Exception.h, Mem.h
+ * @file
+ */
+extern Exception_T STException;
+
+
+#endif
